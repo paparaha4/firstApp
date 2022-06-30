57d500c0e0ca2e1c41d5a712c847e8e3deb950db
paparaha4 <61214392+paparaha4@users.noreply.github.com>
2022-06-30 21:10:05 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 🆂🅸🅽🆇🆄🅻🅵🅸 𝔔𝔞𝔯 on 6/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  Shared
+//
+//  Created by 🆂🅸🅽🆇🆄🅻🅵🅸 𝔔𝔞𝔯 on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
